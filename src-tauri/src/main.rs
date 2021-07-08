eea6a7d449fdcab1e9ba142fc73c756990be0680
Jacob Quant <jacobq@gmail.com>
2021-07-08 17:15:20 -0500

    WIP: splash screen not working

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,17 @@
   windows_subsystem = "windows"
 )]
 
+#[tauri::command(with_window)]
+async fn close_splashscreen<M: tauri::Params>(window: tauri::Window<M>) {
+  if let Ok(splashscreen) = window.get_webview("splash") {
+    splashscreen.close().unwrap();
+  }
+  window.get_webview("main").unwrap().show().unwrap();
+}
+
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![close_splashscreen])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
